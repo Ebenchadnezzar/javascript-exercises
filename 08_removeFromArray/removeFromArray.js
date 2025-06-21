79c0962acd1025912481694b8a5e6f80963c6b7e
Ebenchadnezzar <168885806+Ebenchadnezzar@users.noreply.github.com>
2025-06-21 14:07:28 -0700

    Exercise 9

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (let arg of args) {
+        const argCheck = (element) => element === arg;
+
+        let index = arr.findIndex(argCheck);
+        while (index >= 0) {
+            arr.splice(index, 1);
+            index = arr.findIndex(argCheck);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
