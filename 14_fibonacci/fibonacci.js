83f4da454207b49779b7e97772fe8508ecddbee4
Ebenchadnezzar <168885806+Ebenchadnezzar@users.noreply.github.com>
2025-06-28 11:46:55 -0700

    Exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (typeof(num) == 'string') { num = +num; }
+    if (num < 0) { return 'OOPS'; }
+    if (num == 0) { return 0; }
+    if (num <= 2) { return 1; }
+    
+    let prev = 1;
+    let cur = 2;
 
+    for (let i = 4; i <= num; ++i) {
+        let temp = cur;
+        cur = cur + prev;
+        prev = temp;
+    }
+
+    return cur;
 };
 
 // Do not edit below this line
