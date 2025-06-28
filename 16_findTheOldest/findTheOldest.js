30ccfafb63b927f9912f10950e0e30f8d269ef92
Ebenchadnezzar <168885806+Ebenchadnezzar@users.noreply.github.com>
2025-06-28 12:06:03 -0700

    Exercise 16 (Final Exercise)

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = null;
+    let oldestAge = 0;   
 
+    for (let person of people) {
+        if (!("yearOfDeath" in person)) { person.yearOfDeath = 2025; }
+        let age = person.yearOfDeath - person.yearOfBirth;
+
+        if (age > oldestAge) {
+            oldest = person;
+            oldestAge = age;
+        }
+    }
+
+    return {
+        name: oldest.name,
+        yearOfBirth: oldest.yearOfBirth,
+        yearOfDeath: oldest.yearOfDeath,
+    };
 };
 
 // Do not edit below this line
