402980257e7d462d917f34474bec5097c8787af3
Ebenchadnezzar <168885806+Ebenchadnezzar@users.noreply.github.com>
2025-06-28 11:39:06 -0700

    Exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Lowercase it and remove it's puncuation and spaces
+    // Convert it to an array
+    // check if it is equal to it.reverse()
 
+    str = str.toLowerCase();
+    str = str.replaceAll(',', '');
+    str = str.replaceAll('!', '');
+    str = str.replaceAll('\'', '');
+    str = str.replaceAll('.', '');
+    str = str.replaceAll(' ', '');
+    
+    let original = str.split('');
+    let reversed = original.toReversed();
+
+    let length = (original.length > reversed.length) ? original.length : reversed.length;
+    for (let i = 0; i < length; ++i) {
+        if (original[i] !== reversed[i]) { return false; }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
