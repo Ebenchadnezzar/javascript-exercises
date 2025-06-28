5ebbef53412769063d909887ccdf786b1e66aec4
Ebenchadnezzar <168885806+Ebenchadnezzar@users.noreply.github.com>
2025-06-28 11:52:07 -0700

    Exercise 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((titles, cur) => {
+        titles.push(cur.title);
+        return titles;
+    }, [])
 };
 
 // Do not edit below this line
